e2621c3f06a01e1d612722b859d267eb788999c3
MD. ABU SAYED <64466834+t0wk1r@users.noreply.github.com>
2022-07-21 01:08:50 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+
+$mark = isset($_POST["num"]) ? $_POST["num"] : NULL;
+
+function test()
+{
+
+    global $mark;
+
+    if ($mark < 0 || $mark > 100) {
+        return "Invalid number";
+    } else if ($mark >= 80) {
+        return "apni a+ paisen";
+    } else if ($mark >= 70) {
+        return "apni a paisen";
+    } else if ($mark >= 60) {
+        return "apni a- paisen";
+    } else if ($mark >= 50) {
+        return "apni b paisen";
+    } else if ($mark >= 40) {
+        return "apni c paisen";
+    } else if ($mark >= 33) {
+        return "apni D paisen";
+    } else if ($mark > 0 && $mark < 33) {
+        return "apni fail korsen";
+    } else {
+        return "";
+    }
+}
+$result = test();
+?>
+
+<form action="?" method="post">
+    <input type="text" name="num"><br>
+    <input type="submit">
+</form>
+
+<h1><?php echo $result ?></h1>
